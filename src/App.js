6a6c45b09de678715fdf7dc2b63d8ef10c94e3a4
filthyningen <malikliddell@gmail.com>
2020-10-12 16:18:38 -0400

    Aboutme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container bg-light p-5 mt-5">
+    <div class="row">
+        <div class="col-12">
+            <h1>
+                About Me
+            </h1>
+        </div>
     </div>
+    <div class="row">
+        <div class="col-12">
+            {/* <img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/s960x960/64180540_2249098028518525_1401609306570227712_o.jpg?_nc_cat=106&_nc_sid=e007fa&_nc_ohc=YnldfsU6ZwEAX-PMHz7&_nc_ht=scontent-atl3-1.xx&_nc_tp=7&oh=75fb1f49deff5f45351d78e2e1139081&oe=5F460DE6" 
+            alt="family photo" 
+            class="float-left mr-4"
+             /> */}
+            One thing that most people don't get to see, is my entire name: Orlando Malik Ramon Liddell-Warfield.
+            Before you ask, yes, I had to bubble my whole name during standardized tests.
+            I was born and raised in the Carlinas, which is also where I met my wonderful wife Lea Knox Liddell. We
+            are a happy family with 2 girls, a boy, 2 dogs, and 2 cats.
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            Before I was blessed with a Covid layoff, I was driving a forklift for about 4 years. Once I had the
+            oppurtunity, I decided to continue my education and change my family's life. I pray for a day where I
+            can focus more on my family's future and less on tedious things like bills.
+        </div>
+
+    </div>
+</div>
+
   );
 }
 
